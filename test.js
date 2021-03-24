dc00e2db57987d5eb30a36899425de2c573106a5
Konrad Roguszewski <aker.hm@gmail.com>
2021-03-24 12:28:02 +0100

    added objects to test.js

--- test.js
+++ test.js
@@ -1,5 +1,24 @@
-var string1 = "Abc";
-var string2 = "Bcd";
+var myArray = new Array();
+myArray[0] = 8;
+myArray[1] = "hello";
 
-console.log(string1 < string2);
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Shaun";
+myCar.drive = function(){
+    console.log("now driving");};
 
+myCar.drive();
+
+var myCar2 = {
+
+    maxSpeed: 70,
+    driver: "Net Ninja",
+    drive: function(speed, time){
+        console.log(speed * time);
+    }
+
+};
+
+console.log(myCar2.maxSpeed);
+myCar2.drive(50,3);
